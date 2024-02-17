0874305dfbab52a2271a9bd08773768659c6902b
sawa <you@example.com>
2024-02-17 16:52:02 +0900

    Initial Commit

---
+++ LandmarkRen1/LandmarkRen1App.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkRen1App.swift
+//  LandmarkRen1
+//
+//  Created by Apple on 2024/02/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkRen1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
